43eac773f7a68d59e8023344fefc854fc01df273
Guilherme Iscaro <iscaro@profusion.mobi>
2020-02-28 11:52:47 -0300

    feat(lib): add NodeGateway
    This commit adds the NodeGateway and supporting files in order
    an Apollo federation gateway be able to export delay's node
    interface.

---
+++ lib/service/schemas/index.ts
@@ -0,0 +1,16 @@
+import gql from 'graphql-tag';
+import { DocumentNode } from 'graphql';
+
+export const createNodeQuery = (
+  directives?: string | null,
+): DocumentNode => gql`
+  extend type Query {
+    node(id: ID!): Node ${directives || ''}
+  }
+`;
+
+export const NodeInterface = gql/* GraphQL */ `
+  interface Node {
+    id: ID!
+  }
+`;
