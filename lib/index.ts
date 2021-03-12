3cbc6b985432db94e72de66566be3c1c5af75cd4
Guilherme Iscaro <iscaro@profusion.mobi>
2021-03-12 13:36:42 -0300

    feat: abandon path aliases
    Alias in TS is a mess, since when you build the project
    it won't convert the alias to relative paths, thus requiring
    you to use external tools to do this job.
    In order to avoid such headache, this commit completly
    ditches the aliases and use good old relative
    imports.

--- lib/index.ts
+++ lib/index.ts
@@ -1,8 +1,8 @@
-import resolvers from '@service/resolvers';
-import { createNodeQuery, NodeInterface } from '@service/schemas';
+import resolvers from './service/resolvers';
+import { createNodeQuery, NodeInterface } from './service/schemas';
 
 const { createNodeResolver } = resolvers;
 
 export { createNodeResolver, createNodeQuery, NodeInterface };
-export { default as NodeGateway } from '@gateway/NodeGateway';
-export { default as TypeIDDataSource } from '@utils/TypeIDDataSource';
+export { default as NodeGateway } from './gateway/NodeGateway';
+export { default as TypeIDDataSource } from './utils/TypeIDDataSource';
